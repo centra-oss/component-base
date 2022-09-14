0886519358a959dbe6e89ccb8255cf4681832ab2
Hayden Young <hayden@hbjy.dev>
2022-09-14 18:21:13 +0100

    docs: add example code to doc

--- logs/logs.go
+++ logs/logs.go
@@ -6,6 +6,9 @@ import (
 )
 
 // NewLogger creates a new log.Logger which sends logs to stdout.
+//
+//  log := logs.NewLogger()
+//  log.Println("Hello, world!")
 func NewLogger(prefix string) *log.Logger {
     return log.New(os.Stdout, prefix, 0)
 }
