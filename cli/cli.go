af8c2b2e05b4600333c290d7478ea22da45b3e1e
Hayden Young <hayden@hbjy.dev>
2022-09-15 18:34:18 +0100

    feat: use new logging structure in cli

--- cli/cli.go
+++ cli/cli.go
@@ -1,25 +1,33 @@
 package cli
 
 import (
-	"log"
+	"fmt"
 	"math/rand"
+	"os"
 	"time"
 
 	"github.com/spf13/cobra"
+	"go.h4n.io/centra/component-base/logs"
+	"k8s.io/klog/v2"
 )
 
 // Run provides common boilerplate code around executing a cobra command.
 func Run(cmd *cobra.Command) int { 
-    if err := run(cmd); err != nil {
-        log.Fatalf("command failed: %v", err)
+    if logsInitialized, err := run(cmd); err != nil {
+        if !logsInitialized {
+            fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+        } else {
+            klog.ErrorS(err, "command failed")
+        }
         return 1
     }
 
     return 0
 }
 
-func run(cmd *cobra.Command) (err error) {
+func run(cmd *cobra.Command) (logsInitialized bool, err error) {
     rand.Seed(time.Now().UnixNano())
+    defer logs.FlushLogs()
 
     // In all cases, errors are handled below.
     cmd.SilenceErrors = true
@@ -28,17 +36,23 @@ func run(cmd *cobra.Command) (err error) {
     case cmd.PersistentPreRun != nil:
         pre := cmd.PersistentPreRun
         cmd.PersistentPreRun = func (cmd *cobra.Command, args []string) {
+			logs.InitLogs()
+			logsInitialized = true
             pre(cmd, args)
         }
 
     case cmd.PersistentPreRunE != nil:
         pre := cmd.PersistentPreRunE
         cmd.PersistentPreRunE = func (cmd *cobra.Command, args []string) error {
+			logs.InitLogs()
+			logsInitialized = true
             return pre(cmd, args)
         }
 
     default:
         cmd.PersistentPreRun = func (cmd *cobra.Command, args []string) {
+			logs.InitLogs()
+			logsInitialized = true
         }
     }
 
